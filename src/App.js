273a65bfa4980b300f237ae5872aac0441915c3f
Błażej Kasperski <kasperski.blazej@gmail.com>
2019-09-24 22:45:24 +0200

    Prepared fetch() for data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { createMuiTheme, IconButton, Paper, InputBase } from '@material-ui/core'
+import { createMuiTheme, IconButton, Paper, InputBase, ListItem } from '@material-ui/core'
 import { ThemeProvider } from '@material-ui/styles'
 import './App.css'
 import { amber, red } from '@material-ui/core/colors'
@@ -19,16 +19,27 @@ const theme = createMuiTheme ({
 class App extends Component {
 
   state = {
+    list: null
   }
 
+  handleChange = () => {
+    const url = 'https://api.github.com/search/repositories?q=react&sort=stars&order=desc'
+    fetch(url)
+    .then(response => response.json())
+    .then(data => this.setState ({ list: data}))
+    }
+    
+
   render() {
+
+    console.log(this.state.list)
 
     return (
       <div className="container">
         <ThemeProvider theme={theme}>
 
           <Paper>
-            <IconButton aria-label="menu">
+            <IconButton onClick={this.handleChange} aria-label="menu">
               <MenuIcon />
             </IconButton>
             <InputBase
