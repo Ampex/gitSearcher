93e6f802b3752816c8233e866733990f0d5613d7
Błażej Kasperski <kasperski.blazej@gmail.com>
2019-09-24 17:08:48 +0200

    optimizing general code

--- src/App.js
+++ src/App.js
@@ -1,102 +1,320 @@
 import React, { Component } from 'react'
-import { Button, CircularProgress, Grow } from '@material-ui/core'
+import { Button, LinearProgress, Grow, Snackbar, ExpansionPanel, ExpansionPanelSummary, ExpansionPanelDetails, Typography } from '@material-ui/core'
 import './App.css'
-
-const Lists = props => {
-  return (
-    <p>{props.value}: <span className='status-idle'>{JSON.stringify(props.data)}</span></p>
-  )
-}
-
-const Item = props => (
-  <Grow in timeout={600}>
-    <div>
-      <h3>{props.name}</h3>
-      <code>{props.url}</code>
-      <p>{props.createdAt}</p>
-    </div>
-  </Grow>
-)
+import * as firebase from 'firebase'
+import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator'
+import ExitToAppIcon from '@material-ui/icons/ExitToApp'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 
 class App extends Component {
 
   state = {
-    variant: 'contained',
-    color: 'primary',
-    buttonText: 'Pobierz',
-    items: [],
-    isLoaded: false,
-    isDisabled: false,
-    loading: null
+    disabled: false,
+    email: '',
+    pass: '',
+    currentUser: false,
+    error: false,
+    isOpen: false,
+    logged: false,
   }
-  
-  handleConnect = () => {
 
-    const MongoClient = require('mongodb').MongoClient
-    const uri = "mongodb+srv://blazej:BKdesign06451741@amped-tznfi.mongodb.net/test?retryWrites=true&w=majority"
-    const client = new MongoClient(uri, { useNewUrlParser: true })
-    client.connect(err => {
-      const collection = client.db("test").collection("devices")
-      // perform actions on the collection object
-      client.close()
+  // FIREBASE FUNCTIONS
+  // //login
+  // const auth = firebase.auth()
+  // auth.signInWithEmailAndPassword(email, pass)
+  // //create user
+  // auth.createUserWithEmailAndPassword(email, pass)
+  // //modyfikacja
+  // auth.onAuthStateChange(firebaseUser => {})
+
+  // LOGIN
+  handleLogIn = e => {
+    const { email, pass, disabled, isOpen } = this.state
+
+    this.setState ({
+      [e.target.name]: e.target.value,
+      disabled: !disabled,
+      error: null
     })
 
-  }
-
-  handleGet = () => {
-
-    if (this.state.isLoaded) {
-      console.log('Already loaded')
-    } else {
-
-      const url = 'https://api.github.com/search/repositories?q=react&sort=stars&order=desc'
-
-      fetch(url, this.setState ({ loading: true, isDisabled: true}))
-      .then(response => response.json())
-      .then(data => {
-        
+    setTimeout(()=> {
+      
+      const auth = firebase.auth()
+      const promise = auth.signInWithEmailAndPassword(email, pass)
+      if (firebase.auth().currentUser) {
+        promise.then((e) => this.setState ({
+          currentUser: firebase.auth().currentUser.email,
+          isOpen: !isOpen,
+          disabled: false,
+          logged: true
+        }))
+      } else {
         this.setState ({
-          items: data.items,
-          isLoaded: true,
-          isDisabled: false,
-          loading: false
+          disabled: false,
         })
-      })
-      .catch(error => console.log(error))
+        promise.catch(e => this.setState ({
+          error: e.message,
+        }))
       }
+    }, 3000)
   }
+
+  // REGISTER
+  handleRegister = e => {
+
+    const { email, pass, disabled, isOpen } = this.state
+    
+        this.setState ({
+          error: null,
+          [e.target.name]: e.target.value,
+          disabled: !disabled
+        })
+        setTimeout(()=> {
+
+          const auth = firebase.auth()
+          const promise = auth.createUserWithEmailAndPassword(email, pass)
+          promise.catch(e => this.setState ({ error: e.message }))
+    
+          firebase.auth().onAuthStateChanged(firebaseUser => {
+            if(firebaseUser) {
+              // console.log(firebaseUser)
+              this.setState ({
+                error: null,
+                currentUser: firebase.auth().currentUser.email,
+                isOpen: !isOpen,
+                disabled: false,
+                logged: true
+              })
+            } else {
+              this.setState ({
+                disabled: false
+              })
+            }
+          })
+        }, 2000)
+        
+  }
+
+  // LOGOUT
+  handleLogOut = () => {
+
+    this.setState ({
+      disabled: !this.state.disabled
+    })
+
+    setTimeout(()=> {
+      this.setState ({
+        currentUser: false,
+        disabled: false,
+        logged: false,
+        email: '',
+        pass: '',
+        error: null
+      })
+      firebase.auth().signOut()
+    }, 2000)
+    
+  }
+  
+  // componentDidMount() {
+  //   const rootRef = firebase.database().ref().child('01')
+  //   const emailRef = rootRef.child('email')
+  //   const passRef = rootRef.child('pass')
+  //   emailRef.on('value', snap => {
+  //     this.setState ({
+  //       email: snap.val()
+  //     })
+  //   })
+  //   passRef.on('value', snap => {
+  //     this.setState ({
+  //       pass: snap.val()
+  //     })
+  //   })
+    
+  // }
 
   render() {
 
-    const items = this.state.items    
-    const list = items.map(item => <Item key={item.id} name={item.name} url={item.html_url} createdAt={item.created_at}/>)
+    const { email, pass, currentUser, error, disabled, isOpen, register, logged } = this.state
 
     return (
       <div className="container">
 
-        <Button
-        disabled={this.state.isDisabled}
-        onClick={this.handleGet}
-        variant={this.state.variant}
-        color={this.state.color}
-        >{this.state.buttonText}
+        {disabled && <div style={{position: 'absolute', width: '100%', top: 0}}><LinearProgress /></div>}
+
+        {!currentUser ?
+        // Start form section
+        <React.Fragment>
+        <h1>Login / Register</h1>
+        <p>demo@demo.com</p>
+        <p style={{marginBottom: '25px'}}>password: test1234</p>
+        <ValidatorForm
+        ref='form'
+        onSubmit={this.handleLogIn}
+        // onError={errors => console.log(errors)}
+        >
+
+          <TextValidator
+          disabled={disabled}
+          onChange={e=>this.setState({[e.target.name]:e.target.value})}
+          name='email'
+          value={email}
+          variant='outlined'
+          color='primary'
+          label='E-mail'
+          margin='normal'
+          fullWidth={true}
+          validators={['required', 'isEmail']}
+          errorMessages={['this field is required', 'email is not valid']}
+          ></TextValidator>
+
+          <TextValidator
+          disabled={disabled}
+          type='password'
+          onChange={e=>this.setState({[e.target.name]:e.target.value})}
+          name='pass'
+          value={pass}
+          variant='outlined'
+          color='primary'
+          label='Password'
+          margin='normal'
+          fullWidth={true}
+          validators={['minStringLength:6', 'maxStringLength:16']}
+          errorMessages={['this field is required', 'password is not valid']}
+          ></TextValidator>
+
+          <Button
+          disabled={disabled}
+          type='submit'
+          variant='contained'
+          color='primary'
+          size='large'
+          fullWidth={true}
+          >Sign In
+          </Button>
+
+          <Button
+          disabled={disabled}
+          onClick={this.handleRegister}
+          variant='outlined'
+          color='secondary'
+          size='medium'
+          fullWidth={true}
+          >Register
+          </Button>
+
+        </ValidatorForm>
+ 
+      <Grow in={!disabled}><div>{error && <p className='error'>{error}</p>}</div></Grow>
+      {/* End form section */}
+      </React.Fragment>
+      :
+      <div className='container'>
+        <h2>Hi, {currentUser}</h2>
+        <ExpansionPanel>
+        <ExpansionPanelSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>Account Informations</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+            sit amet blandit leo lobortis eget.
+          </Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+      <ExpansionPanel>
+        <ExpansionPanelSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2a-content"
+          id="panel2a-header"
+        >
+          <Typography>Product Sales / Statistics</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+            sit amet blandit leo lobortis eget.
+          </Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+      <ExpansionPanel>
+        <ExpansionPanelSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>Settings</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+            sit amet blandit leo lobortis eget.
+          </Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+      <ExpansionPanel disabled>
+        <ExpansionPanelSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>VIP Options</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+            sit amet blandit leo lobortis eget.
+          </Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+        <div className='footer'>© 2019 bkasperski</div>
+      </div>
+      
+      
+    }
+      {/* FOOTER */}
+      <Grow in={logged}>
+        <div className='user'>
+        {currentUser ?
         
-        {this.state.loading && <CircularProgress style={{color: 'rgb(76, 175, 80)', 'position': 'absolute'}} size={24} />}
-
-        </Button>
-
-        <Button
-        onClick={this.handleConnect}
-        variant={this.state.variant}
-        color='secondary'
-        >Połącz
         
-        </Button>
+        <React.Fragment>
+          <span>Logged as: </span>
+          {currentUser}
+          <Button
+          id='register'
+          disabled={disabled}
+          onClick={this.handleLogOut}
+          variant='contained'
+          color='secondary'
+          size='small'
+          >
+            <ExitToAppIcon/>
+          </Button>
+        </React.Fragment> : null
 
-        <Lists value={'Status'} data={this.state.isLoaded}/>
-        <Lists value={'Ilość obiektów'} data={this.state.items.length}/>
+        }
+        </div>
+      </Grow>
 
-        <div className='list'>{list}</div>
+      <Snackbar
+      open={isOpen}
+      message={<span>Login successful</span>}
+      anchorOrigin={{vertical: 'bottom', horizontal: 'left'}}
+      autoHideDuration={3500}
+      onClose={()=> this.setState ({ isOpen: !isOpen })}
+      ></Snackbar>
+      <Snackbar
+      open={register}
+      message={<span>Register and Login successful</span>}
+      anchorOrigin={{vertical: 'bottom', horizontal: 'left'}}
+      autoHideDuration={3500}
+      onClose={()=> this.setState ({ register: !register })}
+      ></Snackbar>
 
       </div>
 
