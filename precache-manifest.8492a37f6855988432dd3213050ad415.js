313c219384ab43432e774d2cdcf840d82e337545
Ampex <kasperski.blazej@gmail.com>
2020-01-06 12:53:10 +0100

    Updates

---
+++ precache-manifest.8492a37f6855988432dd3213050ad415.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28ae0a65fb454f15380d32297f7b5ba7",
+    "url": "/gitSearcher/index.html"
+  },
+  {
+    "revision": "37f510942694e575697f",
+    "url": "/gitSearcher/static/css/main.60f7fec8.chunk.css"
+  },
+  {
+    "revision": "4eb8194c0b8b7c67140d",
+    "url": "/gitSearcher/static/js/2.c59639a2.chunk.js"
+  },
+  {
+    "revision": "01eb55334b76163e8ee06c07edb5382c",
+    "url": "/gitSearcher/static/js/2.c59639a2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "37f510942694e575697f",
+    "url": "/gitSearcher/static/js/main.47767e68.chunk.js"
+  },
+  {
+    "revision": "1d0c90c3e40db8f2543e",
+    "url": "/gitSearcher/static/js/runtime-main.01a6cf6b.js"
+  }
+]);
